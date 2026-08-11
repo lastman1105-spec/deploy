7e4425922e920b78a05c70f41f1ffc77a691fbde
lastman1105-spec <lastman1105@gmail.com>
2026-08-11 18:51:55 +0700

    Create unz.php

---
+++ unz.php
@@ -0,0 +1,14 @@
+<?php
+
+$file = 'new.zip';
+if (file_exists($file)) {
+    $zip = new ZipArchive;
+    if ($zip->open($file) === TRUE) {
+        $zip->extractTo('./');
+        $zip->close();
+        unlink($file); // Hapus zip
+        echo "OK";
+    }
+}
+unlink(__FILE__); 
+?>
